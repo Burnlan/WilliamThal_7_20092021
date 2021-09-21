cf89967677736f74c7e3f525c52679a0cc5f9801
William <william.thal.67@gmail.com>
2021-09-21 17:20:17 +0200

    created toggle between signup and login

--- src/components/ConnectionForm.vue
+++ src/components/ConnectionForm.vue
@@ -1,12 +1,15 @@
 <template>
+<div>
+    <form>
+        <label for="emailInput" class="form-label">Adresse email</label>
+        <input type="email" id="emailInput" class="form-control">
+        <label for="pwdInput" class="form-label">Mot de passe</label>
+        <input type="password" class="form-control" id="pwdInput">
+        <button v-if="signup" type="submit" class="btn btn-primary mt-3 end">Créer un compte</button>
+        <button v-else type="submit" class="btn btn-primary mt-3 end">Se connecter</button>
+    </form>
+</div>
 
-<form>
-    <label for="emailInput" class="form-label">Adresse email</label>
-    <input type="email" id="emailInput" class="form-control">
-    <label for="pwdInput" class="form-label">Mot de passe</label>
-    <input type="password" class="form-control" id="pwdInput">
-    <button type="submit" class="btn btn-primary mt-3 end">Créer un compte</button>
-</form>
 
 
 
@@ -14,7 +17,10 @@
 
 <script>
 export default {
-
+    name: "ConnectionForm",
+    props: {
+        signup: { type: Boolean }
+    }
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="home">
-    <ConnectionForm />
+<div class="container-sm">
+  <div class="btn-group con-selector mb-3" role="group">
+    <button @click="select('signup')" class="btn btn-outline-primary" ref="signbtn">S'inscrire</button>
+    <button @click="select('login')" class="btn btn-outline-primary" ref="logbtn">Se connecter</button>
   </div>
+
+  <ConnectionForm :signup="signup"/>
+
+</div>
+
+  
 </template>
 
 <script>
@@ -11,7 +19,35 @@ export default {
   name: 'Home',
   components: {
     ConnectionForm
+  },
+  data(){
+    return {
+      signup: false
+    }
+  },
+  methods: {
+    select(btn) {
+      if(btn == "signup") {
+        this.$refs.logbtn.classList.remove("active");
+        this.$refs.signbtn.classList.add("active");
+        this.signup = true;
+      } else {
+        this.$refs.signbtn.classList.remove("active");
+        this.$refs.logbtn.classList.add("active");
+        this.signup = false;
+      }
+    console.log(this.signup);
+    }
   }
 }
 
 </script>
+
+<style lang="scss">
+.con-selector {
+  display: flex;
+}
+
+
+
+</style>
