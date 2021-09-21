f5affdf8dc470a32877b2f433bfd0af23ae00e40
William <william.thal.67@gmail.com>
2021-09-21 18:06:57 +0200

    created 2 distinct forms

--- src/components/ConnectionForm.vue
+++ src/components/ConnectionForm.vue
@@ -1,12 +1,22 @@
 <template>
 <div>
-    <form>
+    <form v-if="signup">
+        <label for="firstnameInput" class="form-label">Prénom</label>
+        <input type="text" id="firstnameInput" class="form-control">
+        <label for="lastnameInput" class="form-label">Nom</label>
+        <input type="text" id="lastnameInput" class="form-control">
         <label for="emailInput" class="form-label">Adresse email</label>
         <input type="email" id="emailInput" class="form-control">
         <label for="pwdInput" class="form-label">Mot de passe</label>
         <input type="password" class="form-control" id="pwdInput">
-        <button v-if="signup" type="submit" class="btn btn-primary mt-3 end">Créer un compte</button>
-        <button v-else type="submit" class="btn btn-primary mt-3 end">Se connecter</button>
+        <button type="submit" class="btn btn-primary mt-3 end">Créer un compte</button>
+    </form>
+    <form v-else>
+        <label for="emailInput" class="form-label">Adresse email</label>
+        <input type="email" id="emailInput" class="form-control">
+        <label for="pwdInput" class="form-label">Mot de passe</label>
+        <input type="password" class="form-control" id="pwdInput">
+        <button type="submit" class="btn btn-primary mt-3 end">Se connecter</button>
     </form>
 </div>
 
