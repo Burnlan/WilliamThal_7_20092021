0b58f7e3a850e6b5e16da6ff7b39998544e682b6
William <william.thal.67@gmail.com>
2021-10-07 12:47:41 +0200

    added regex and form validtion on signup

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,14 +1,17 @@
 <template>
-    <form class="d-flex flex-column">
+    <form class="d-flex flex-column" ref="signupform">
         <label for="firstnameInput" class="form-label">Prénom</label>
-        <input type="text" id="firstnameInput" class="form-control" v-model="signupdata.firstname">
+        <input type="text" id="firstnameInput" class="form-control" required v-model="signupdata.firstname">
         <label for="lastnameInput" class="form-label">Nom</label>
-        <input type="text" id="lastnameInput" class="form-control" v-model="signupdata.lastname"> 
+        <input type="text" id="lastnameInput" class="form-control" required v-model="signupdata.lastname"> 
         <label for="emailInput" class="form-label">Adresse email</label>
-        <input type="email" id="emailInput" class="form-control" v-model="signupdata.email">
+        <input type="email" id="emailInput" class="form-control" required v-model="signupdata.email">
         <label for="pwdInput" class="form-label">Mot de passe</label>
-        <input type="password" class="form-control" id="pwdInput" v-model="signupdata.password">
-        <button @click="registerUser" class="btn btn-primary mt-3 align-self-end">Créer un compte</button>
+        <input type="password" class="form-control" :class="{ 'is-invalid': pwdWarning }" id="pwdInput" required v-model="signupdata.password">
+        <div id="passwordHelp" class="form-text">
+            <p>Votre mot de passe dois contenir au moins une lettre miniscule, une lettre majuscule, un chiffre et faire plus de 8 caractères</p>
+        </div>
+        <button @click.prevent="registerUser" type="submit" class="btn btn-primary mt-3 align-self-end">Créer un compte</button>
     </form>
 </template>
 
@@ -18,13 +21,22 @@ export default {
     data() {
         return {
             //this is the object that will create a new user, we get it from our form using vue model
-            signupdata: { firstname: "", lastname: "", email: "", password: "" }
+            signupdata: { firstname: "", lastname: "", email: "", password: "" },
+            pwdOk: false,
+            pwdWarning: false,
+            isValid: false
         }
     },
     methods: {
-        async registerUser(e) {
-            e.preventDefault();
-            let response = await fetch("http://localhost:3000/api/signup", {
+        async registerUser() {
+            //we let the browser inform the user of what's wrong
+            this.$refs.signupform.reportValidity();
+            //we test the pwd againsdt our regex
+            const reg = new RegExp("^(?=.*)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$");
+            this.pwdOk = reg.test(this.signupdata.password);
+            //if the form is valid and the password passes the regex, we proceed
+            if(this.$refs.signupform.checkValidity() && this.pwdOk){
+                let response = await fetch("http://localhost:3000/api/signup", {
                 method: "POST",
                 headers: {
                     "Accept": 'application/json', 
@@ -32,16 +44,20 @@ export default {
                 },
                 //Sends the data in json format
                 body: JSON.stringify(this.signupdata)
-            })
-            if(response.ok){
-                console.log("User "+this.signupdata.firstname+" successfully created !");
-                this.$emit("signedup");
+                })
+                if(response.ok){
+                    console.log("User "+this.signupdata.firstname+" successfully created !");
+                    this.$emit("signedup");
+                } else {
+                    console.log(response);
+                }
             } else {
-                console.log(response);
+                if(!this.pwdOk){
+                    this.pwdWarning = true;
+                }
             }
-        }
-        
-    }
+        },
+    },
 }
 </script>
 
